8e991eadfa7b7c4b7bab2adf9d3073d5f7bfbdb5
Sumitha V <HappyGowda23@gmail.com>
2025-03-16 15:08:16 +0530

    first commit

---
+++ CSharpThreadingBasic/Program.cs
@@ -0,0 +1,124 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region single thread example
+// single thread example
+//Method1();
+//Method2();
+//Method3();
+#endregion 
+
+#region multi thread example
+// multi thread example
+
+//Console.WriteLine("Main Thread Started");
+////Creating Threads
+//Thread t1 = new Thread(Method1)
+//{
+//    Name = "Thread1"
+//};
+//Thread t2 = new Thread(Method2)
+//{
+//    Name = "Thread2"
+//};
+//Thread t3 = new Thread(Method3)
+//{
+//    Name = "Thread3"
+//};
+////Executing the methods
+//t1.Start();
+//t2.Start();
+//t3.Start();
+
+//Console.WriteLine("All the methods processed");
+#endregion
+
+#region async programming with sequentially process
+// async programming with sequentially process
+//Console.WriteLine("Main execution started " + Thread.CurrentThread.ManagedThreadId);
+//await Method1Async();
+//await Method2Async();
+//await Method3Async();
+#endregion 
+
+#region async programming with concurrent process 
+// async programming with concurrent process 
+var method1Task = Method1Async();
+var method2Task = Method2Async();
+var method3Task = Method3Async();
+
+await Task.WhenAll(method1Task, method2Task, method3Task);
+
+Console.WriteLine("Complete it");
+#endregion
+
+static async Task Method1Async()
+{
+    Console.WriteLine("Method1 Started using " + Thread.CurrentThread.ManagedThreadId);
+    for (int i = 1; i <= 5; i++)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("Method1 delayed processed " + Thread.CurrentThread.ManagedThreadId);
+        Console.WriteLine("Method1 :" + i);
+    }
+    Console.WriteLine("Method1 Ended using " + Thread.CurrentThread.ManagedThreadId);
+}
+static async Task Method2Async()
+{
+    Console.WriteLine("Method2 Started using " + Thread.CurrentThread.ManagedThreadId);
+    for (int i = 1; i <= 5; i++)
+    {
+        if (i == 3)
+        {
+            Console.WriteLine("Performing the Database Operation Started");
+            await Task.Delay(100000);
+            Console.WriteLine("Performing the Database Operation Completed");
+        }
+    }
+    Console.WriteLine("Method2 Ended using " + Thread.CurrentThread.ManagedThreadId);
+}
+static async Task Method3Async()
+{
+    Console.WriteLine("Method3 Started using " + Thread.CurrentThread.ManagedThreadId);
+    for (int i = 1; i <= 5; i++)
+    {
+        await Task.Delay(1);
+        Console.WriteLine("Method3 :" + i);
+    }
+    Console.WriteLine("Method3 Ended using " + Thread.CurrentThread.ManagedThreadId);
+}
+
+static void Method1()
+{
+    Console.WriteLine("Method1 Started using " + Thread.CurrentThread.Name);
+    for (int i = 1; i <= 5; i++)
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Method1 :" + i);
+    }
+    Console.WriteLine("Method1 Ended using " + Thread.CurrentThread.Name);
+}
+static void Method2()
+{
+    Console.WriteLine("Method2 Started using " + Thread.CurrentThread.Name);
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.WriteLine("Method2 :" + i);
+        if (i == 3)
+        {
+            Console.WriteLine("Performing the Database Operation Started");
+            //Sleep for 10 seconds
+            Thread.Sleep(10000);
+            Console.WriteLine("Performing the Database Operation Completed");
+        }
+    }
+    Console.WriteLine("Method2 Ended using " + Thread.CurrentThread.Name);
+}
+static void Method3()
+{
+    Console.WriteLine("Method3 Started using " + Thread.CurrentThread.Name);
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.WriteLine("Method3 :" + i);
+    }
+    Console.WriteLine("Method3 Ended using " + Thread.CurrentThread.Name);
+}
